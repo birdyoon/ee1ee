823fa62c27923e97536629f74db634e29c39c672
Yu ByungMoon <qudans1237@gmail.com>
2022-04-16 17:42:16 +0900

    04-16 #3

---
+++ 01.Model2MVCShop(stu)/src/main/java/com/model2/mvc/view/purchase/UpdatePurchaseAction.java
@@ -0,0 +1,40 @@
+package com.model2.mvc.view.purchase;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.model2.mvc.framework.Action;
+import com.model2.mvc.service.purchase.PurchaseService;
+import com.model2.mvc.service.purchase.impl.PurchaseServiceImpl;
+import com.model2.mvc.service.purchase.vo.PurchaseVO;
+import com.model2.mvc.service.user.vo.UserVO;
+
+public class UpdatePurchaseAction extends Action {
+
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response) throws Exception { 
+	
+		int tranNo= Integer.parseInt(request.getParameter("tranNo"));
+		
+		HttpSession session = request.getSession();
+		UserVO userVO = (UserVO)session.getAttribute("user");
+				
+		PurchaseVO purchaseVO=new PurchaseVO();
+		purchaseVO.setTranNo(tranNo);
+		purchaseVO.setBuyer(userVO);
+		purchaseVO.setPaymentOption(request.getParameter("paymentOption"));
+		purchaseVO.setReceiverName(request.getParameter("receiverName"));
+		purchaseVO.setReceiverPhone(request.getParameter("receiverPhone"));
+		purchaseVO.setDivyAddr(request.getParameter("divyAddr"));
+		purchaseVO.setDivyRequest(request.getParameter("divyRequest"));
+		purchaseVO.setDivyDate(request.getParameter("divyDate"));
+		
+		PurchaseService service=new PurchaseServiceImpl();
+		service.updatePurchase(purchaseVO);
+	
+		
+	
+		return "forward:/updatePurchase.jsp";
+	}
+}
